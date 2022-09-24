24602ef07dbbcf1673c333e3c236b19b7305d2ec
thanhtrinhtv <trinhthanhit@gmail.com>
2022-09-24 20:35:13 +0700

    one

---
+++ app/src/main/java/com/example/tinhdoanapp/Model/UnionModel.java
@@ -0,0 +1,4 @@
+package com.example.tinhdoanapp.Model;
+
+public class UnionModel {
+}
---
+++ app/src/main/java/com/example/tinhdoanapp/Model/UnitModel.java
@@ -0,0 +1,8 @@
+package com.example.tinhdoanapp.Model;
+
+public class UnitModel {
+    int Id;
+    String Name;
+    String Address;
+    int Parent;
+}
